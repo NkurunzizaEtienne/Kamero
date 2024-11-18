7ec48cf94278ea36b47676822a4bba4aeb5c055c
NKURUNZIZA@Etienne <136488521+NkurunzizaEtienne@users.noreply.github.com>
2024-11-19 00:25:21 +0200

    Add files via upload

---
+++ LastPage/app.js
@@ -0,0 +1,8 @@
+tinymce.init({
+  selector: '#myEditor',
+  plugins: 'lists link image code media',
+  toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | bullist numlist outdent indent | link image media | code',
+  height: 300,
+  branding: false,
+  
+})
